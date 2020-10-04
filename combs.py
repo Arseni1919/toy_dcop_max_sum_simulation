1a0f2f99a117f86eed3147727877b2fa1546d027
Arseni <1919ars@gmail.com>
2020-10-04 14:32:44 +0300

    update

--- combs.py
+++ combs.py
@@ -4,12 +4,15 @@ a = [1,3,4]
 b = [2,3,5]
 c = [3,4,5]
 
+import operator
+stats = {'a':1000, 'b':3000, 'c': 100, 5: 500000}
+print(max(stats.items(), key=operator.itemgetter(1))[0], max(stats.items(), key=operator.itemgetter(1))[1])
 # for i in collections.Counter(a).items():
 #     print(i[1])
 # for order, i in enumerate(itertools.product(*[a, b, c])):
 #     print(order)
 #     print(i)
 
-list_of_domains = [[1,3,4], [2,3,5], [3,4,5]]
-for i in itertools.product(*list_of_domains):
-    print(i)
+# list_of_domains = [[1,3,4], [2,3,5], [3,4,5]]
+# for i in itertools.product(*list_of_domains):
+#     print(i)
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from pprint import pprint
+import operator
 import itertools
 import collections
 from prettytable import PrettyTable
-ITERATIONS = 20
+from termcolor import colored
+import random
+ITERATIONS = 12
 
 
 class FunctionNode:
@@ -16,7 +19,8 @@ class FunctionNode:
     def _create_message(self, var_node):
         message = {}
         for d in var_node.domain:
-            message[d] = 9999999
+            message[d] = -9999999
+            # message[d] = 0
         return message
 
     def _create_list_of_domains(self, var_node):
@@ -42,11 +46,11 @@ class FunctionNode:
         list_of_domains, order_of_nei = self._create_list_of_domains(var_node)
         # print(list_of_domains)
         for i in itertools.product(*list_of_domains):
-            # print(i)
-            # if i[0] == 3 and var_node.name == 'robot1' and self.name == 'cell3':
-            #     print(i, self.func(self, i, order_of_nei) + self._prev_iter_brings(i, order_of_nei, iteration))
-            message[i[0]] = min(message[i[0]], self.func(self, i, order_of_nei) +
-                                self._prev_iter_brings(i, order_of_nei, iteration))
+            # print(f'{self.name}: for {i[0]} option: {self.func(self, i, order_of_nei) + self._prev_iter_brings(i, order_of_nei, iteration)}')
+            # print(f'{self.name} -> {var_node.name}: with comb:{i} the func value: {self.func(self, i, order_of_nei)}, the added value:{self._prev_iter_brings(i, order_of_nei, iteration)}')
+            message[i[0]] = max(message[i[0]],
+                                (self.func(self, i, order_of_nei) +
+                                self._prev_iter_brings(i, order_of_nei, iteration)))
         flatten_message(message)
         var_node.message_box[iteration][self.name] = message
 
@@ -54,6 +58,8 @@ class FunctionNode:
 class VariableNode:
     def __init__(self, name, domain):
         self.name = name
+        self.num = int(name[len('robot'):])
+        # print(f'{self.name}: {self.num}')
         self.domain = domain
         self.neighbours = []
         self.message_box = {}
@@ -73,7 +79,7 @@ class VariableNode:
                     past_message = self.message_box[iteration-1][nei.name]
                     for d in self.domain:
                         message[d] += past_message[d]
-
+        flatten_message(message)
         func_node.message_box[iteration][self.name] = message
 
 
@@ -92,28 +98,87 @@ def send_message(agents, iteration):
             agent.send_message_to(nei, iteration)
 
 
-def func_target1(self, combination, order_of_nei):
-    count = 0
-    me = combination[0]
-    if me in [1,2,3]:
-        others = combination[1:]
+def create_func_target(cells_near_me):
+    def func_target(self, combination, order_of_nei):
+        count = 0
+        domain_choice_of_var = combination[0]
+        # if domain_choice_of_var in cells_near_me:
+        # others = combination[1:]
+        others = combination
+        # if len(set(others)) < len(others):
+        #     return 0
         for i in others:
-            if i in [1,2,3]:
+            if i in cells_near_me:
                 count += 1
-        return min(30, 100 - 30*count)
-    return 0
+        if count == 0:
+            return 0
+        return max(0, min(100, 30 * count))
+        # return 0
+    return func_target
 
 
 def func_cell(self, combination, order_of_nei):
-    if combination[0] == self.num:
-        counter = 0
-        for i in collections.Counter(combination).items():
-            if i[0] == self.num:
-                counter = max(counter, i[1])
-        if counter > 1:
-            return -30 * counter
+    counter = 0
+
+    for item_and_how_many_of_it in collections.Counter(combination).items():
+        # looking only on one item and it th cell itself
+        if item_and_how_many_of_it[0] == self.num:
+            counter = max(counter, item_and_how_many_of_it[1])
+    if counter > 1:
+        return -30 * counter  # + (0.5 - random.random())
+
+    if self.num in combination:
+        i = combination.index(self.num)
+        if len(order_of_nei) <= 1:
+            return 0
+            # return order_of_nei[i].num * 2
+        aaa = order_of_nei[i].num / 10 * self.num
+        return order_of_nei[i].num / 10 * self.num
+
     return 0
 
+
+def func_cell1(self, combination, order_of_nei):
+    comb = []
+    for t in order_of_nei:
+        comb.append(combination[t.num - 1])
+
+    my_dict = {
+        (1, 1): -30,
+        (1, 2): 0.1,
+        (2, 1): 0.2,
+        (2, 2): 0
+    }
+    return my_dict[tuple(comb)]
+
+
+def func_cell2(self, combination, order_of_nei):
+    comb = []
+    for t in order_of_nei:
+        comb.append(combination[t.num - 1])
+
+    my_dict = {
+        (1, 1): 0,
+        (1, 2): 0.3,
+        (2, 1): 0.4,
+        (2, 2): -30
+    }
+    return my_dict[tuple(comb)]
+
+
+
+def func_cell3(self, combination, order_of_nei):
+    comb = []
+    for t in order_of_nei:
+        comb.append(combination[t.num - 1])
+
+    my_dict = {
+        (3, 3): -30,
+        (3, 9): 0.1,
+        (8, 3): 0.3,
+        (8, 9): 0
+    }
+    return my_dict[tuple(comb)]
 
 def flatten_message(message):
     min_value = 99999
@@ -136,79 +201,338 @@ def print_table_of_messages(all_agents, iteration):
             if b.name in a.message_box[iteration]:
                 cell_to_print = ''
                 for k, v in a.message_box[iteration][b.name].items():
-                    cell_to_print = cell_to_print + str(k) + '->' + str(v) + '\n'
+                    cell_to_print = cell_to_print + str(k) + '->' + str(round(v, 2)) + '\n'
                 raw.append(cell_to_print)
             else:
                 raw.append('')
         table.add_row(raw)
-    print('### ITERATION: %s ###' % iteration)
+
+    print('---')
+    print('### ITERATION: %s ###' % (iteration + 1))
     print(table)
+
+    for a in all_agents:
+        if 'robot' in a.name:
+            counter_dict = {}
+            for d in a.domain:
+                counter_dict[d] = 0
+            for b in all_agents:
+                if b.name in a.message_box[iteration]:
+                    for k, v in a.message_box[iteration][b.name].items():
+                        counter_dict[k] += v
+
+            max_value = max(counter_dict.values())
+            cells_with_highest_value = [k for k, v in counter_dict.items() if v == max_value]
+            print(colored(a.name, 'green'), end=' ')
+            choose_str = 'chooses one of' if len(cells_with_highest_value) > 1 else 'chooses'
+            print(f'{choose_str}: {cells_with_highest_value}', end=' ')
+            print(f'with the highest value: {max_value}')
 
 
 def scenario_1():
     # init
-    target1 = FunctionNode('targ1', func_target1, 1)
-    cell1 = FunctionNode('cell1', func_cell, 1)
-    cell2 = FunctionNode('cell2', func_cell, 2)
-    cell3 = FunctionNode('cell3', func_cell, 3)
-    cell4 = FunctionNode('cell4', func_cell, 4)
-    cell5 = FunctionNode('cell5', func_cell, 5)
-    robot1 = VariableNode('robot1', [1, 3, 4])
-    robot2 = VariableNode('robot2', [2, 3, 5])
-    robot3 = VariableNode('robot3', [3, 4, 5])
+    target1 = FunctionNode('targ1', create_func_target(cells_near_me=[3]), num=1)
+    cell3 = FunctionNode('cell3', func_cell, num=3)
+    cell8 = FunctionNode('cell8', func_cell, num=8)
+    cell9 = FunctionNode('cell9', func_cell, num=9)
+    robot1 = VariableNode('robot1', domain=[3, 8])
+    robot2 = VariableNode('robot2', domain=[3, 9])
 
-    target1.neighbours = [robot1, robot2, robot3]
-    cell1.neighbours = [robot1]
-    cell2.neighbours = [robot2]
-    cell3.neighbours = [robot1, robot2, robot3]
-    cell4.neighbours = [robot1, robot3]
-    cell5.neighbours = [robot2, robot3]
-    robot1.neighbours = [cell1, cell3, cell4, target1]
-    robot2.neighbours = [cell2, cell3, cell5, target1]
-    robot3.neighbours = [cell3, cell4, cell5, target1]
+    target1.neighbours = [robot1, robot2]
+    cell3.neighbours = [robot1, robot2]
+    cell8.neighbours = [robot1]
+    cell9.neighbours = [robot2]
+    robot1.neighbours = [cell8, cell3, target1]
+    robot2.neighbours = [cell9, cell3, target1]
 
-    return [target1, cell1, cell2, cell3, cell4, cell5, robot1, robot2, robot3]
+    return [target1, cell3, cell8, cell9, robot1, robot2]
 
 
 def scenario_2():
     # init
-    target1 = FunctionNode('targ1', func_target1, 1)
-    cell1 = FunctionNode('cell1', func_cell, 1)
-    cell2 = FunctionNode('cell2', func_cell, 2)
-    cell3 = FunctionNode('cell3', func_cell, 3)
-    cell4 = FunctionNode('cell4', func_cell, 4)
-    cell5 = FunctionNode('cell5', func_cell, 5)
-    robot1 = VariableNode('robot1', [1, 3, 4])
-    robot2 = VariableNode('robot2', [2, 3, 5])
+    target1 = FunctionNode('targ1', create_func_target(cells_near_me=[3]), num=1)
+
+    cell3 = FunctionNode('cell3', func_cell, num=3)
+    cell8 = FunctionNode('cell8', func_cell, num=8)
+    cell9 = FunctionNode('cell9', func_cell, num=9)
+    cell10 = FunctionNode('cell10', func_cell, num=10)
+
+    robot1 = VariableNode('robot1', domain=[3, 8])
+    robot2 = VariableNode('robot2', domain=[3, 9])
+    robot3 = VariableNode('robot3', domain=[3, 10])
+
+    target1.neighbours = [robot1, robot2, robot3]
+    cell3.neighbours = [robot1, robot2, robot3]
+    cell8.neighbours = [robot1]
+    cell9.neighbours = [robot2]
+    cell10.neighbours = [robot3]
+
+    robot1.neighbours = [cell8, cell3, target1]
+    robot2.neighbours = [cell9, cell3, target1]
+    robot3.neighbours = [cell10, cell3, target1]
+
+    return [target1, cell3, cell8, cell9, cell10, robot1, robot2, robot3]
+
+
+def scenario_3():
+    # init
+    target1 = FunctionNode('targ1', create_func_target(cells_near_me=[3,8,9]), num=1)
+
+    cell3 = FunctionNode('cell3', func_cell, num=3)
+    cell8 = FunctionNode('cell8', func_cell, num=8)
+    cell9 = FunctionNode('cell9', func_cell, num=9)
+    cell10 = FunctionNode('cell10', func_cell, num=10)
+
+    robot1 = VariableNode('robot1', domain=[3, 8])
+    robot2 = VariableNode('robot2', domain=[3, 9])
+    robot3 = VariableNode('robot3', domain=[3, 10])
+
+    # neighbours
+    target1.neighbours = [robot1, robot2, robot3]
+
+    cell3.neighbours = [robot1, robot2, robot3]
+    cell8.neighbours = [robot1]
+    cell9.neighbours = [robot2]
+    cell10.neighbours = [robot3]
+
+    robot1.neighbours = [cell8, cell3, target1]
+    robot2.neighbours = [cell9, cell3, target1]
+    robot3.neighbours = [cell10, cell3, target1]
+
+    return [target1, cell3, cell8, cell9, cell10, robot1, robot2, robot3]
+
+
+def scenario_4():
+    # print(scenario_4.__name__)
+    # init
+    target1 = FunctionNode('targ1', create_func_target(cells_near_me=[3]), num=1)
+
+    cell3 = FunctionNode('cell3', func_cell, num=3)
+    cell4 = FunctionNode('cell4', func_cell, num=4)
+    cell5 = FunctionNode('cell5', func_cell, num=5)
+    cell8 = FunctionNode('cell8', func_cell, num=8)
+    cell9 = FunctionNode('cell9', func_cell, num=9)
+    cell10 = FunctionNode('cell10', func_cell, num=10)
+
+    robot1 = VariableNode('robot1', domain=[3, 4, 8])
+    robot2 = VariableNode('robot2', domain=[3, 5, 9])
+    robot3 = VariableNode('robot3', domain=[3, 4, 5, 10])
+
+    # neighbours
+    target1.neighbours = [robot1, robot2, robot3]
+
+    cell3.neighbours = [robot1, robot2, robot3]
+    cell4.neighbours = [robot1, robot3]
+    cell5.neighbours = [robot2, robot3]
+    cell8.neighbours = [robot1]
+    cell9.neighbours = [robot2]
+    cell10.neighbours = [robot3]
+
+    robot1.neighbours = [cell8, cell4, cell3, target1]
+    robot2.neighbours = [cell9, cell5, cell3, target1]
+    robot3.neighbours = [cell10, cell5, cell4, cell3, target1]
+
+    return [target1, cell3, cell4, cell5, cell8, cell9, cell10, robot1, robot2, robot3]
+
+
+def scenario_5():
+    # init
+    target1 = FunctionNode('targ1', create_func_target(cells_near_me=[3,8,9]), num=1)
+
+    cell3 = FunctionNode('cell3', func_cell, num=3)
+    cell4 = FunctionNode('cell4', func_cell, num=4)
+    cell5 = FunctionNode('cell5', func_cell, num=5)
+    cell8 = FunctionNode('cell8', func_cell, num=8)
+    cell9 = FunctionNode('cell9', func_cell, num=9)
+    cell10 = FunctionNode('cell10', func_cell, num=10)
+
+    robot1 = VariableNode('robot1', domain=[3, 4, 8])
+    robot2 = VariableNode('robot2', domain=[3, 5, 9])
+    robot3 = VariableNode('robot3', domain=[3, 4, 5, 10])
+
+    # neighbours
+    target1.neighbours = [robot1, robot2, robot3]
+
+    cell3.neighbours = [robot1, robot2, robot3]
+    cell4.neighbours = [robot1, robot3]
+    cell5.neighbours = [robot2, robot3]
+    cell8.neighbours = [robot1]
+    cell9.neighbours = [robot2]
+    cell10.neighbours = [robot3]
+
+    robot1.neighbours = [cell8, cell4, cell3, target1]
+    robot2.neighbours = [cell9, cell5, cell3, target1]
+    robot3.neighbours = [cell10, cell5, cell4, cell3, target1]
+
+    return [target1, cell3, cell4, cell5, cell8, cell9, cell10, robot1, robot2, robot3]
+
+
+def scenario_6():
+    # init
+    target1 = FunctionNode('targ1', create_func_target(cells_near_me=[3]), num=1)
+
+    cell3 = FunctionNode('cell3', func_cell, num=3)
+    cell5 = FunctionNode('cell5', func_cell, num=5)
+    cell8 = FunctionNode('cell8', func_cell, num=8)
+    cell9 = FunctionNode('cell9', func_cell, num=9)
+    cell10 = FunctionNode('cell10', func_cell, num=10)
+
+    robot1 = VariableNode('robot1', domain=[3, 8])
+    robot2 = VariableNode('robot2', domain=[3, 5, 9])
+    robot3 = VariableNode('robot3', domain=[3, 5, 10])
+
+    # neighbours
+    target1.neighbours = [robot1, robot2, robot3]
+
+    cell3.neighbours = [robot1, robot2, robot3]
+    cell5.neighbours = [robot2, robot3]
+    cell8.neighbours = [robot1]
+    cell9.neighbours = [robot2]
+    cell10.neighbours = [robot3]
+
+    robot1.neighbours = [cell8, cell3, target1]
+    robot2.neighbours = [cell9, cell5, cell3, target1]
+    robot3.neighbours = [cell10, cell5, cell3, target1]
+
+    return [target1, cell3, cell5, cell8, cell9, cell10, robot1, robot2, robot3]
+
+
+def scenario_7():
+    # init
+    target1 = FunctionNode('targ1', create_func_target(cells_near_me=[3, 8, 9]), num=1)
+
+    cell3 = FunctionNode('cell3', func_cell, num=3)
+    cell5 = FunctionNode('cell5', func_cell, num=5)
+    cell8 = FunctionNode('cell8', func_cell, num=8)
+    cell9 = FunctionNode('cell9', func_cell, num=9)
+    cell10 = FunctionNode('cell10', func_cell, num=10)
+
+    robot1 = VariableNode('robot1', domain=[3, 8])
+    robot2 = VariableNode('robot2', domain=[3, 5, 9])
+    robot3 = VariableNode('robot3', domain=[3, 5, 10])
+
+    # neighbours
+    target1.neighbours = [robot1, robot2, robot3]
+
+    cell3.neighbours = [robot1, robot2, robot3]
+    cell5.neighbours = [robot2, robot3]
+    cell8.neighbours = [robot1]
+    cell9.neighbours = [robot2]
+    cell10.neighbours = [robot3]
+
+    robot1.neighbours = [cell8, cell3, target1]
+    robot2.neighbours = [cell9, cell5, cell3, target1]
+    robot3.neighbours = [cell10, cell5, cell3, target1]
+
+    return [target1, cell3, cell5, cell8, cell9, cell10, robot1, robot2, robot3]
+
+def scenario_8_1():
+    # init
+    # target1 = FunctionNode('targ1', create_func_target(cells_near_me=[3, 8, 9]), num=1)
+
+    cell1 = FunctionNode('cell1', func_cell1, num=1)
+    cell2 = FunctionNode('cell2', func_cell2, num=2)
+
+    robot1 = VariableNode('robot1', domain=[1, 2])
+    robot2 = VariableNode('robot2', domain=[1, 2])
+
+    # neighbours
+    cell1.neighbours = [robot1, robot2]
+    cell2.neighbours = [robot1, robot2]
+
+
+    robot1.neighbours = [cell1, cell2]
+    robot2.neighbours = [cell1, cell2]
+
+    return [cell1, cell2, robot1, robot2]
+
+def scenario_8_2():
+    # init
+    # target1 = FunctionNode('targ1', create_func_target(cells_near_me=[3, 8, 9]), num=1)
+
+    cell1 = FunctionNode('cell1', func_cell, num=1)
+    cell2 = FunctionNode('cell2', func_cell, num=2)
+    cell8 = FunctionNode('cell8', func_cell, num=8)
+    cell9 = FunctionNode('cell9', func_cell, num=9)
+
+    robot1 = VariableNode('robot1', domain=[1, 2, 8])
+    robot2 = VariableNode('robot2', domain=[1, 2, 9])
+
+    # neighbours
+    cell1.neighbours = [robot1, robot2]
+    cell2.neighbours = [robot1, robot2]
+    cell8.neighbours = [robot1]
+    cell9.neighbours = [robot2]
+
+    robot1.neighbours = [cell1, cell2, cell8]
+    robot2.neighbours = [cell1, cell2, cell9]
+
+    return [cell1, cell2, cell8, cell9, robot1, robot2]
+
+
+def scenario_9():
+    # init
+    target1 = FunctionNode('targ1', create_func_target(cells_near_me=[3]), num=1)
+    target2 = FunctionNode('targ2', create_func_target(cells_near_me=[3]), num=2)
+    cell3 = FunctionNode('cell3', func_cell, num=3)
+    cell8 = FunctionNode('cell8', func_cell, num=8)
+    cell9 = FunctionNode('cell9', func_cell, num=9)
+    robot1 = VariableNode('robot1', domain=[3, 8])
+    robot2 = VariableNode('robot2', domain=[3, 9])
 
     target1.neighbours = [robot1, robot2]
-    cell1.neighbours = [robot1]
-    cell2.neighbours = [robot2]
+    target2.neighbours = [robot1, robot2]
     cell3.neighbours = [robot1, robot2]
-    cell4.neighbours = [robot1]
-    cell5.neighbours = [robot2]
-    robot1.neighbours = [cell1, cell3, cell4, target1]
-    robot2.neighbours = [cell2, cell3, cell5, target1]
+    cell8.neighbours = [robot1]
+    cell9.neighbours = [robot2]
+    robot1.neighbours = [cell8, cell3, target1, target2]
+    robot2.neighbours = [cell9, cell3, target1, target2]
 
-    return [target1, cell1, cell2, cell3, cell4, cell5, robot1, robot2]
+    return [target1, target2, cell3, cell8, cell9, robot1, robot2]
+
+
+def scenario_10():
+    # init
+    target1 = FunctionNode('targ1', create_func_target(cells_near_me=[3]), num=1)
+    cell3 = FunctionNode('cell3', func_cell3, num=3)
+    cell8 = FunctionNode('cell8', func_cell, num=8)
+    cell9 = FunctionNode('cell9', func_cell, num=9)
+    robot1 = VariableNode('robot1', domain=[3, 8])
+    robot2 = VariableNode('robot2', domain=[3, 9])
+
+    target1.neighbours = [robot1, robot2]
+    cell3.neighbours = [robot1, robot2]
+    cell8.neighbours = [robot1]
+    cell9.neighbours = [robot2]
+    robot1.neighbours = [cell8, cell3, target1]
+    robot2.neighbours = [cell9, cell3, target1]
+
+    return [target1, cell3, cell8, cell9, robot1, robot2]
 
 
 def main():
     # init
-    # all_agents = scenario_1()
-    all_agents = scenario_2()
+    # scenario_func = scenario_1
+    # scenario_func = scenario_2
+    # scenario_func = scenario_3
+    # scenario_func = scenario_4
+    # scenario_func = scenario_5
+    # scenario_func = scenario_6
+    # scenario_func = scenario_7
+    scenario_func = scenario_8_1
+    # scenario_func = scenario_8_2
+    # scenario_func = scenario_9
+    # scenario_func = scenario_10
+
+    all_agents = scenario_func()
+    print(scenario_func.__name__)
 
     init_message_boxes(all_agents)
 
     for iteration in range(ITERATIONS):
         send_message(all_agents, iteration)
-        # pprint(robot3.message_box[iteration])
-        # print()
-        # pprint(cell3.message_box[iteration])
-        print('---')
         print_table_of_messages(all_agents, iteration)
 
 
 if __name__ == '__main__':
     main()
-
